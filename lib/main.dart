69d91a57f106a52ef719575e0af30923915c31a4
Nils Backe <nilsbacke@nilss-mbp.lan>
2019-05-18 11:56:51 -0400

    android working need to enable firebase in code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() {
   // Set `enableInDevMode` to true to see reports while in debug mode
   // This is only to be used for confirming that reports are being
   // submitted as expected. It is not intended to be used for everyday
   // development.
-  Crashlytics.instance.enableInDevMode = true;
+  // Crashlytics.instance.enableInDevMode = true;
 
-  // Pass all uncaught errors to Crashlytics.
-  FlutterError.onError = (FlutterErrorDetails details) {
-    Crashlytics.instance.onError(details);
-  };
+  // // Pass all uncaught errors to Crashlytics.
+  // FlutterError.onError = (FlutterErrorDetails details) {
+  //   Crashlytics.instance.onError(details);
+  // };
   runApp(App());
 }
